0d722bbea6e4f21fb1c44e0017967841514fcb4e
dylanvanwinkle <dylanvanwinkle@gmail.com>
2021-04-21 18:33:00 -0700

    ETDIP

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -23,12 +23,60 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	private static int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private static float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private static String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private static Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	
+	public static int getItemsReceived(){
+		return itemsReceived;
+	}
+	
+	public static float getdegreesTurned(){
+		return degreesTurned;
+	}
+	
+	public static String getnomenclature(){
+		return nomenclature;
+	}
+	
+	public static Object getmemberObj(){
+		return memberObj;
+	}
+	
+	public static void setItemsReceived(int newValue) {
+		if(newValue >= 0){
+			itemsReceived = newValue;
+		}else {
+			itemsReceived = 0;
+		}
+	}
+	
+	public static void setdegreesTurned(float newValue){
+	if (newValue >= 0 && newValue <= 360) {
+		degreesTurned = newValue;
+	}else {
+		System.out.println("Degrees turned must be beetween 0 and 360.");
+	} 	
+	}
+	
+	public static void setnomenclature(String newValue){
+	if (newValue != "") {
+		nomenclature = newValue;
+	}else {
+		nomenclature = " ";
+	}	
+	}
+	
+	public static void setmemberObj(Object newValue){
+		if (newValue instanceof String) {
+			memberObj = new Object();
+		}else {
+			memberObj = newValue;
+		}
+	}
 	
 	public static void main(String[] args) {
-		
+		 
 	}
 }
---
+++ src/_01_introduction_to_encapsulation/EncapsulateTheDataTest.java
@@ -0,0 +1,52 @@
+package _01_introduction_to_encapsulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EncapsulateTheDataTest {
+
+	@Test
+	void test() {
+		EncapsulateTheData.setItemsReceived(0);
+		assertEquals(0, EncapsulateTheData .getItemsReceived());
+		EncapsulateTheData.setItemsReceived(-1);
+		assertNotEquals(-1, EncapsulateTheData .getItemsReceived());
+		EncapsulateTheData.setItemsReceived(-10);
+		assertEquals(0, EncapsulateTheData .getItemsReceived());
+	}
+	@Test
+	void test2(){
+		EncapsulateTheData.setdegreesTurned(0);
+		assertEquals(0, EncapsulateTheData .getdegreesTurned());
+		EncapsulateTheData.setdegreesTurned(360);
+		assertEquals(360, EncapsulateTheData .getdegreesTurned());
+		EncapsulateTheData.setdegreesTurned(-10);
+		assertNotEquals(-10, EncapsulateTheData .getdegreesTurned());
+		EncapsulateTheData.setdegreesTurned(400);
+		assertNotEquals(400, EncapsulateTheData .getdegreesTurned());
+	}
+
+	@Test
+	void test3(){
+		EncapsulateTheData.setnomenclature("Hello");
+		assertEquals("Hello", EncapsulateTheData.getnomenclature());
+		EncapsulateTheData.setnomenclature(" ");
+		assertEquals(" ", EncapsulateTheData.getnomenclature());
+		EncapsulateTheData.setnomenclature("");
+		assertEquals(" ", EncapsulateTheData.getnomenclature());
+	}
+	
+	@Test
+	void test4(){
+		EncapsulateTheData.setmemberObj(1);
+		assertEquals(1, EncapsulateTheData.getmemberObj());
+		EncapsulateTheData.setmemberObj(true);
+		assertEquals(true, EncapsulateTheData.getmemberObj());
+		EncapsulateTheData.setmemberObj("");
+		asserttrue(EncapsulateTheData.memberObj instanceOf Object);
+		EncapsulateTheData.setmemberObj("Hello");
+		assertEquals(new Object(), EncapsulateTheData.getmemberObj());
+		
+	}
+}
